fe22c436a3fa6f5141c392734ed3ae56ddcd9307
iropatich <iropatich@gylgroup.com>
2026-04-29 09:56:13 -0300

    Feature/2904/Creo entidad y repository de tipo de producto

---
+++ src/main/java/com/gyl/CrudGyl/entity/TipoProducto.java
@@ -0,0 +1,26 @@
+package com.gyl.CrudGyl.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tiposProducto")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TipoProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 100)
+    private String descripcion;
+}
---
+++ src/main/java/com/gyl/CrudGyl/repository/TipoProductoRepository.java
@@ -0,0 +1,7 @@
+package com.gyl.CrudGyl.repository;
+
+import com.gyl.CrudGyl.entity.TipoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoProductoRepository extends JpaRepository<TipoProducto, Long> {
+}
