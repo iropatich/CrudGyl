b94c71d563efb3771658efcac8b6baa3744018e8
iropatich <iropatich@gylgroup.com>
2026-04-30 14:42:42 -0300

    Change/3004/cambio en los ids a GenerationType.IDENTITY

--- src/main/java/com/gyl/CrudGyl/entity/Producto.java
+++ src/main/java/com/gyl/CrudGyl/entity/Producto.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 //esto en poo es encapsulamiento
 public class Producto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 100)
