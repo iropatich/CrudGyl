f84ab296d42c28d317a05661d215968cebd3a872
iropatich <iropatich@gylgroup.com>
2026-04-30 15:16:18 -0300

    Change/3004/Cambios en entidad DetalleVenta

--- src/main/java/com/gyl/CrudGyl/entity/DetalleVenta.java
+++ src/main/java/com/gyl/CrudGyl/entity/DetalleVenta.java
@@ -1,25 +1,36 @@
 package com.gyl.CrudGyl.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "detallesVenta")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DetalleVenta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Integer cantidad;
 
+    @Column(nullable = false)
     private Double precioUnitario;
 
+    @Column(nullable = false)
     private Double subtotal;
 
     @ManyToOne
-    @JoinColumn(name = "idVenta")
+    @JoinColumn(name = "idVenta", nullable = false)
     private Venta venta;
 
     @ManyToOne
-    @JoinColumn(name = "idProducto")
+    @JoinColumn(name = "idProducto", nullable = false)
     private Producto producto;
 }
